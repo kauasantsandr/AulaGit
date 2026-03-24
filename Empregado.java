117d726f8a9243ad526b0204ad175778eae84456
Kauã Andrade <kauaandrade@hotmail.com.br>
2026-03-24 17:46:56 -0300

    First commit - add new class Empregado

---
+++ Empregado.java
@@ -0,0 +1,20 @@
+public class Empregado {
+    private string nome;
+    private int idade;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(Int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+}
