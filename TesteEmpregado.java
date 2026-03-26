95ae0057eef1e856de083ef797db5cba5d01b528
Kauã Andrade <kauaandrade@hotmail.com.br>
2026-03-26 15:34:17 -0300

    atualizando criação

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,6 +1,11 @@
 Pubic class TesteEmpregado{
     public static void main(String[] args) {
-        Empregado empregado1 = new Empregado(nome: "Marco", idade: 30 );
+        String nome "kauã";
+        String apelido = nome;
+        nome: "maria";
+
+        Empregado empregado1 = new Empregado(nome: "maria", idade: 18, salario 5000);
+        
         Empregado empregado2 = empregado1;
         empregado2.setNome(nome: "Ana");
         System.out.printIn(empregado1.getNome());
