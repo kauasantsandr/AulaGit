00de7b31602d3c5283c32ebfa89f95c09053db92
Kauã Andrade <kauaandrade@hotmail.com.br>
2026-03-26 16:14:25 -0300

    ajuste da class TesteEmpregado

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,13 +1,7 @@
 Pubic class TesteEmpregado{
     public static void main(String[] args) {
-        String nome "kauã";
-        String apelido = nome;
-        nome: "maria";
-
-        Empregado empregado1 = new Empregado(nome: "maria", idade: 18, salario 5000);
-        
-        Empregado empregado2 = empregado1;
-        empregado2.setNome(nome: "Ana");
+        Empregado empregado1 = new Empregado(nome: "maria", idade: 18, tipo: 1, salario: 5000, comissao: 0, bonus: 0);
+        Empregado empregado2 = new Empregado(nome: "Ana", idade: 20, tipo: 2, salario: 5000, comissao: 5, bonus: 0);
         System.out.printIn(empregado1.getNome());
         System.out.printIn(empregadp2.getNome());
     }
